073c32b586166e0ae5000e38f78da65ddcc5adab
Daniel Knittl-Frank <knittl89+git@googlemail.com>
2020-04-04 20:37:34 +0200

    Prepare website for github-pages

--- windows/kbdneo2/Quelldateien/kbd_mod.h
+++
@@ -1,7 +0,0 @@
-/****************************************************************************\
-* Module Name: KBD_MOD.H
-* Änderungen an der KBD.H für das deutsches ergonomische Layout Neo 2.0
-\****************************************************************************/
-
-#undef DEADTRANS
-#define DEADTRANS(accent, ch, comp, flags) { MAKELONG(ch, accent), comp, flags}
--- windows/kbdneo2/Treiber/reg_backup.bat
+++
@@ -1,3 +0,0 @@
-start Regedit /e backup1.reg "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Keyboard Layouts\b0000407"
-start Regedit /e backup2.reg "HKEY_LOCAL_MACHINE\SYSTEM\ControlSet002\Control\Keyboard Layouts\b0000407"
-start Regedit /e backup3.reg "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Keyboard Layouts\b0000407"
